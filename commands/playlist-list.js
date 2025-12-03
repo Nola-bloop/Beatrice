00c90d79e67ee8629bb037c178954f0e152e324a
Nola <Nola.Bloop@gmail.com>
2025-12-03 17:47:08 -0500

    test

--- commands/playlist-list.js
+++ commands/playlist-list.js
@@ -4,36 +4,20 @@ import caller from '../API-calls.js';
 export default {
 	data: new SlashCommandBuilder()
 		.setName('playlist-list')
-		.setDescription('Show your playlists')
-        .addStringOption(option =>
-        	option
-        		.setName('name')
-        		.setDescription('Required if <name> is in command description. ')
-        		.setRequired(false)
-        ),
+		.setDescription('Show your playlists'),
 
 	async execute(interaction) {
 		let userId = interaction.member.user.id;
 		let playlists = await caller.ListPlaylists(userId)
 		if (playlists.length > 0){
-			let output = "```"
+			let output = "Playlists:\n```"
 			for (let i = 0; i < playlists.length ; i++){
 				output += `${playlists[i].id}: ${playlists[i].name}\n`
 			}
-			output += "Playlists:\n```"
+			output += "```"
 			await interaction.reply(output)
 		}else{
 			await interaction.reply('No playlist found.')
 		}
-		/*
-		else if (action == "create"){
-			let userId = interaction.member.user.id;
-			let name = interaction.options.getString('name')
-			caller.CreatePlaylist(name, userId)
-			await interaction.reply('Use `/playlist list` to confirm creation.')
-		}
-		else{
-			await interaction.reply('Unkown parameters.')
-		}*/
 	},
 };
---
+++ commands/playlist-new.js
@@ -0,0 +1,21 @@
+import { SlashCommandBuilder } from 'discord.js';
+import caller from '../API-calls.js';
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName('playlist-new')
+		.setDescription('Create a playlist.')
+        .addStringOption(option =>
+        	option
+        		.setName('name')
+        		.setDescription('The name of your new playlist.')
+        		.setRequired(true)
+        ),
+
+	async execute(interaction) {
+		let userId = interaction.member.user.id;
+		let name = interaction.options.getString('name')
+		caller.CreatePlaylist(name, userId)
+		await interaction.reply('Use `/playlist list` to confirm creation.')
+},
+};
